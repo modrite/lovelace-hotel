7d245fcb8395c1ec82ff7ee44d1a7be2b46e7021
modrite <81558821+modrite@users.noreply.github.com>
2021-08-02 21:11:28 +0300

    Initial commit

---
+++ src/main/java/com/group3/lovelacehotel/LovelaceHotelApplication.java
@@ -0,0 +1,13 @@
+package com.group3.lovelacehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelaceHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovelaceHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/lovelacehotel/LovelaceHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.lovelacehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelaceHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
