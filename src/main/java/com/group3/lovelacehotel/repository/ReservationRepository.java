f20aa69b64702f1509c448e65ee28f421221dbca
modrite <81558821+modrite@users.noreply.github.com>
2021-08-13 13:04:51 +0300

    fixed ReservationRepository

--- src/main/java/com/group3/lovelacehotel/repository/ReservationRepository.java
+++ src/main/java/com/group3/lovelacehotel/repository/ReservationRepository.java
@@ -18,8 +18,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query("Select r.room from Reservation  r where " +
             "r.checkOutDate >=:date " +
-            "and r.numberOfAdults =:numberOfAdults " +
-            "and r.numberOfChildren =:numberOfChildren ")
+            "and r.room.numberOfAdults =:numberOfAdults " +
+            "and r.room.numberOfChildren =:numberOfChildren ")
     Set<Room> availableRoomsByParam(@Param("date") LocalDateTime date,
                              @Param("numberOfAdults") Long numberOfAdults,
                              @Param("numberOfChildren") Long numberOfChildren);
